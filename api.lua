7028a7af4ea6fbad426c22c8fb7767f84cfef4bf
A. Demant <ademant@ademant.de>
2018-12-05 05:36:23 +0100

    add some basic handlers

--- api.lua
+++ api.lua
@@ -1,7 +1,11 @@
 
-M=xpfw
-M.experiences={}
-M.players={}
+local M=xpfw.store_table
+if M.player == nil then
+	M.player={}
+end
+if M.experiences == nil then
+	M.experiences={}
+end
 
 local check_value=function(tab,val,def)
 if tab[val] == nil then
@@ -18,18 +22,76 @@ M.register_experience=function(name,indata)
 end
 
 minetest.register_on_joinplayer(function(player)
-	local playername=player:get_player_name()
+	local playername = player:get_player_name()
+
 	if M.player[playername]==nil then
-		M.player[playername].walked=0 -- really walked distances
-		M.player[playername].distance=0 -- including teleport etc.
-		M.player[playername].logon=0 -- logon time
-		M.player[playername].logstat={} -- detailed logons (if configured)
-		M.player[playername].dug=0 -- count of nodes dugged
-		M.player[playername].build=0 -- count of build nodes
-		M.player[playername].deaths=0 -- count of deaths
-		M.player[playername].killed_mobs=0 -- count of killed mobs
-		M.player[playername].killed_player=0 --count of killed players
+		M.player[playername]={walked=0, -- really walked distances
+			distance=0, -- including teleport etc.
+			logon=0, -- logon time
+			logstat={}, -- detailed logons (if configured)
+			dug=0, -- count of nodes dugged
+			build=0, -- count of build nodes
+			deaths=0, -- count of deaths
+			spoke=0, -- count of chat messages
+			killed_mobs=0, -- count of killed mobs
+			killed_player=0, --count of killed players
+			last_pos=player:get_pos(), --actual position
+			}
 	end
-	M.player[playername].lastlogin=os.clock() -- last login time
+	M.player[playername].lastlogin=os.time() -- last login time
+end
+)
+
+minetest.register_on_placenode(function(pos, newnode, player, oldnode, itemstack, pointed_thing)
+	if player ~= nil then
+		local playername = player:get_player_name()
+		local playerdata=M.player[playername]
+		if playerdata ~= nil then
+			playerdata.build=playerdata.build+1
+		end
+	end
+end)
+
+minetest.register_on_dieplayer(function(player, reason)
+	print(dump2(reason))
+	if player ~= nil then
+		local playername = player:get_player_name()
+		local playerdata=M.player[playername]
+		if playerdata ~= nil then
+			playerdata.death=playerdata.death+1
+		end
+	end
+end)
+minetest.register_on_chat_message(function(player, reason)
+	print(dump2(reason))
+	if player ~= nil then
+--		local playername = player:get_player_name()
+		local playerdata=M.player[player]
+		if playerdata ~= nil then
+			playerdata.spoke=playerdata.spoke+1
+		end
+	end
+end)
+
+minetest.register_on_dignode(function(pos,oldnoe,player)
+	local playername = player:get_player_name()
+	local playerdata=M.player[playername]
+	if playerdata ~= nil then
+		playerdata.dug=playerdata.dug+1
+	end
+end)
+
+minetest.register_on_joinplayer(function(player)
+	local playername = player:get_player_name()
+	local playerdata=M.player[playername]
+	if playerdata ~= nil then
+		local leave=os.time()
+		playerdata.logon=playerdata.logon+(leave-playerdata.lastlogin)
+	end
+end)
+
+minetest.register_on_shutdown(function()
+	print(dump2(M))
+	xpfw.mod_storage:from_table(M)
 end
 )
--- init.lua
+++ init.lua
@@ -3,7 +3,10 @@ xpfw = {}
 xpfw.path = minetest.get_modpath("xpfw")
 xpfw.config = minetest.get_mod_storage()
 xpfw.modname=minetest.get_current_modname()
+xpfw.mod_storage=minetest.get_mod_storage()
+xpfw.store_table=xpfw.mod_storage:to_table()
 
+print(dump2(xpfw.mod_store))
 minetest.log("action", "[MOD]"..minetest.get_current_modname().." -- start loading from "..minetest.get_modpath(minetest.get_current_modname()))
 -- Load files
 
