95fa2605fc730a3e1fe7c82b61e5570018349ece
A. Demant <ademant@ademant.de>
2019-01-02 15:31:35 +0100

    add crafting

--- config.lua
+++ config.lua
@@ -14,7 +14,7 @@ xpfw.experience_max=tonumber(minetest.settings:get("xpfw.experience_max")) or 20
 xpfw.rtime=tonumber(minetest.settings:get("xpfw.recreation_time")) or 5
 
 for i,attr in ipairs({"walked","distance","swam","login","dug","build","deaths","spoke","killed_mobs","killed_player",
-		"logon"}) do
+		"logon","craft"}) do
 	xpfw.register_attribute(attr,{min=0,max=math.huge,default=0,hud=1})
 end
 for i,attr in ipairs({"lastlogin"}) do
@@ -25,6 +25,16 @@ xpfw.register_attribute("meanlight",{min=0,max=default.LIGHT_MAX,
 	default=math.min(default.LIGHT_MAX,tonumber(minetest.settings:get("xpfw.default")) or 11),
 	hud=1
 	})
+
+for i,attr in ipairs({"walked","swam","dig","build","craft"}) do
+	xpfw.register_attribute("mean_"..attr.."_speed",{min=0,max=20,
+		moving_average_factor=tonumber(minetest.settings:get("xpfw."..attr.."_mean_weight")) or 100,
+		recreation_factor=(tonumber(minetest.settings:get("xpfw."..attr.."_recreation")) or 50),
+		default=0,
+		hud=1
+		})
+end
+--[[
 xpfw.register_attribute("mean_walked_speed",{min=0,max=20,
 	moving_average_factor=tonumber(minetest.settings:get("xpfw.walked_mean_weight")) or 100,
 	recreation_factor=(tonumber(minetest.settings:get("xpfw.walked_recreation")) or 200),
@@ -49,5 +59,5 @@ xpfw.register_attribute("mean_build_speed",{min=0,max=20,
 	default=0,
 	hud=1
 	})
-
+]]
 --print(dump2(xpfw.attributes))
