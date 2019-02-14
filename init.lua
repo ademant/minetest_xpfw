d752d27df0a5435110d81c311a8e6f0b6e341906
ademant <ademant@ademant.de>
2019-02-14 13:58:13 +0100

    prepared for translations

--- init.lua
+++ init.lua
@@ -5,6 +5,8 @@ xpfw.modname=minetest.get_current_modname()
 xpfw.mod_storage=minetest.get_mod_storage()
 xpfw.store_table={}--xpfw.mod_storage:to_table()
 xpfw.attributes={}
+local S = dofile(xpfw.path .. "/intllib.lua")
+xpfw.intllib = S
 
 --print(dump2(xpfw.mod_store))
 minetest.log("action", "[MOD]"..minetest.get_current_modname().." -- start loading from "..minetest.get_modpath(minetest.get_current_modname()))
